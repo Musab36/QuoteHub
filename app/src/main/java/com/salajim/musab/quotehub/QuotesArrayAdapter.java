d2aa15f09b12cace0fc46883c4f17956679e0e1f
Musab36 <musabhd@gmail.com>
2017-09-09 17:43:56 +0300

    Quotes activity, Intent, ArrayAdapter, tests and some changes.

---
+++ app/src/main/java/com/salajim/musab/quotehub/QuotesArrayAdapter.java
@@ -0,0 +1,32 @@
+package com.salajim.musab.quotehub;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class QuotesArrayAdapter extends ArrayAdapter {
+private Context mContext;
+private String[] mQuotes;
+private String[] mAuthors;
+
+    // ArrayAdapter constructor
+    public QuotesArrayAdapter(Context mContext, int resource, String[] mQuotes, String[] mAuthors) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mQuotes = mQuotes;
+        this.mAuthors = mAuthors;
+    }
+
+    // Getting the Quotes and the Authors
+    @Override
+    public Object getItem(int position) {
+        String quote = mQuotes[position];
+        String author = mAuthors[position];
+        return String.format("%s \n By: %s", quote, author); // Formatting the string and breaking the line between the Quote and the author
+    }
+
+    // Returning all the Quotes
+    @Override
+    public int getCount() {
+        return mQuotes.length;
+    }
+}
