9db3e5717b8aa7591e7b8ef57f1732111dbe8721
Musab36 <musabhd@gmail.com>
2017-09-16 19:05:33 +0300

    API implemented.

--- app/src/main/java/com/salajim/musab/quotehub/QuoteListActivity.java
+++ app/src/main/java/com/salajim/musab/quotehub/QuoteListActivity.java
@@ -3,10 +3,15 @@ package com.salajim.musab.quotehub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,7 +22,7 @@ public class QuoteListActivity extends AppCompatActivity {
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     private QuoteListAdapter mAdapter;
     public ArrayList<Quote> mQuotes = new ArrayList<>();
-
+    /*
     private String[] quotes = new String[] {"Never give up, always keep pushing and believe in yourself", "Don't cry because it's over, smile because it happened.", "Be yourself; everyone else is already taken.",
     "Be the change that you wish to see in the world.", "If you tell the truth, you don't have to remember anything.", "A friend is someone who knows all about you and still loves you.",
     "Always forgive your enemies; nothing annoys them so much.", "Live as if you were to die tomorrow. Learn as if you were to live forever.",
@@ -32,16 +37,55 @@ public class QuoteListActivity extends AppCompatActivity {
     private String[] authors = new String[] {"Musab Abdirahman", "Dr. Seuss", " Oscar Wilde", " Mahatma Gandhi", " Mark Twain", " Elbert Hubbard", " Oscar Wilde",
     "Mahatma Gandhi", " Oscar Wilde", "Mother Teresa", "Stephen G. Weinbaum", "James Joyce", "Rod Williams", "Benjamin Franklin", "Albert Einstein",
     "Robert J. Collier", "Kid President (Robby Novak)", "Mark Twain", "Elna Rae", "Pope John XXIII", "Confucius", "Pablo Picasso", "A. P. J. Abdul Kalam", "LT Abdullahi Dahir"};
-
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
         ButterKnife.bind(this);
 
+        /*
+        // Setting the ArrayAdapter to work with ListView
+        QuoteListAdapter adapter = new QuoteListAdapter(this, android.R.layout.simple_list_item_1, quotes, authors);
+        mRecyclerView.setAdapter(mAdapter);
+        */
+
         Intent intent = getIntent(); // Getting the Intent from the MainActivity
         String quotes = intent.getStringExtra("quotes"); // Pulling the data from the Intent
 
         mQuotesTextView.setText("Here are all the Quote for: " + quotes); // Setting text
+
+        getQuotes(quotes);
+    }
+
+    // Callback method for desplaying response data
+    private void getQuotes(String quotes) {
+        final FavqsService favqsService = new FavqsService(); //// Creating new instance of Favourite Quotes
+        favqsService.findQuotes(quotes, new Callback() {// new Callback represents the second argument in findQuotes() method
+
+            // This method is run when our request fails
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();// prints details of the error to the output console
+            }
+
+            // This method is run if our request is successful
+            @Override
+            public void onResponse(Call call, Response response) {
+                mQuotes = favqsService.processResults(response);// We are triggering .processResults() and collect its return value
+
+                QuoteListActivity.this.runOnUiThread(new Runnable() {//We are switching to UI thread, Runnable helps sharing code between threads
+                    //Runnable method which contains the code for the UI thread
+                    @Override
+                    public void run() {
+                        mAdapter = new QuoteListAdapter(getApplicationContext(), mQuotes);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(QuoteListActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
     }
 }
